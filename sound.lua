7da3cbddcd488d1e20d52d2c50b246514e9e919e
OchdasisjaSuppi <08_15Gamer@posteo.at>
2025-04-12 12:39:42 +0200

    Added 15 new Soundbites

--- sound.lua
+++ sound.lua
@@ -4,45 +4,60 @@ nova.require "data/lua/jh/sound/common"
 
 register_sound "flesh_bullet_critical_hit"
 {
-    group = "fx",
+	group = "fx",
 	volume = 0.8,
-    "data/sound/being/summoner/summoner_melee_01_01.wav",
-    "data/sound/being/summoner/summoner_melee_01_02.wav",
-    "data/sound/being/summoner/summoner_melee_01_03.wav",
+	"data/sound/being/summoner/summoner_melee_01_01.wav",
+	"data/sound/being/summoner/summoner_melee_01_02.wav",
+	"data/sound/being/summoner/summoner_melee_01_03.wav",
+	"data/sound/being/summoner/summoner_melee_01_04.wav",
+	"data/sound/being/summoner/summoner_melee_01_05.wav",
+	"data/sound/being/summoner/summoner_melee_01_06.wav",
 }
 
 register_sound "flesh_blunt_critical_hit"
 {
-    group = "fx",
+	group = "fx",
 	volume = 0.8,
-    "data/sound/event/flesh_blunt_critical_hit/flesh_blunt_critical_hit_01.wav",
-    "data/sound/event/flesh_blunt_critical_hit/flesh_blunt_critical_hit_02.wav",
-    "data/sound/event/flesh_blunt_critical_hit/flesh_blunt_critical_hit_03.wav",
+	"data/sound/event/flesh_blunt_critical_hit/flesh_blunt_critical_hit_01.wav",
+	"data/sound/event/flesh_blunt_critical_hit/flesh_blunt_critical_hit_02.wav",
+	"data/sound/event/flesh_blunt_critical_hit/flesh_blunt_critical_hit_03.wav",
+	"data/sound/event/flesh_blunt_critical_hit/flesh_blunt_critical_hit_04.wav",
+	"data/sound/event/flesh_blunt_critical_hit/flesh_blunt_critical_hit_05.wav",
+	"data/sound/event/flesh_blunt_critical_hit/flesh_blunt_critical_hit_06.wav",
 }
 
 register_sound "metal_blunt_critical_hit"
 {
-    group = "fx",
+	group = "fx",
 	volume = 0.8,
-    "data/sound/event/metal_blunt_critical_hit/metal_blunt_critical_hit_01.wav",
-    "data/sound/event/metal_blunt_critical_hit/metal_blunt_critical_hit_02.wav",
-    "data/sound/event/metal_blunt_critical_hit/metal_blunt_critical_hit_03.wav",
+	"data/sound/event/metal_blunt_critical_hit/metal_blunt_critical_hit_01.wav",
+	"data/sound/event/metal_blunt_critical_hit/metal_blunt_critical_hit_02.wav",
+	"data/sound/event/metal_blunt_critical_hit/metal_blunt_critical_hit_03.wav",
+	"data/sound/event/metal_blunt_critical_hit/metal_blunt_critical_hit_04.wav",
+	"data/sound/event/metal_blunt_critical_hit/metal_blunt_critical_hit_05.wav",
+	"data/sound/event/metal_blunt_critical_hit/metal_blunt_critical_hit_06.wav",
 }
 
 register_sound "flesh_blade_critical_hit"
 {
-    group = "fx",
+	group = "fx",
 	volume = 0.8,
-    "data/sound/event/flesh_blade_critical_hit/flesh_blade_critical_hit_01.wav",
-    "data/sound/event/flesh_blade_critical_hit/flesh_blade_critical_hit_02.wav",
-    "data/sound/event/flesh_blade_critical_hit/flesh_blade_critical_hit_03.wav",
+	"data/sound/event/flesh_blade_critical_hit/flesh_blade_critical_hit_01.wav",
+	"data/sound/event/flesh_blade_critical_hit/flesh_blade_critical_hit_02.wav",
+	"data/sound/event/flesh_blade_critical_hit/flesh_blade_critical_hit_03.wav",
+	"data/sound/event/flesh_blade_critical_hit/flesh_blade_critical_hit_04.wav",
+	"data/sound/event/flesh_blade_critical_hit/flesh_blade_critical_hit_05.wav",
+	"data/sound/event/flesh_blade_critical_hit/flesh_blade_critical_hit_06.wav",
 }
 
 register_sound "metal_blade_critical_hit"
 {
-    group = "fx",
+	group = "fx",
 	volume = 0.8,
-    "data/sound/event/metal_blade_critical_hit/metal_blade_critical_hit_01.wav",
-    "data/sound/event/metal_blade_critical_hit/metal_blade_critical_hit_02.wav",
-    "data/sound/event/metal_blade_critical_hit/metal_blade_critical_hit_03.wav",
+	"data/sound/event/metal_blade_critical_hit/metal_blade_critical_hit_01.wav",
+	"data/sound/event/metal_blade_critical_hit/metal_blade_critical_hit_02.wav",
+	"data/sound/event/metal_blade_critical_hit/metal_blade_critical_hit_03.wav",
+	"data/sound/event/metal_blade_critical_hit/metal_blade_critical_hit_04.wav",
+	"data/sound/event/metal_blade_critical_hit/metal_blade_critical_hit_05.wav",
+	"data/sound/event/metal_blade_critical_hit/metal_blade_critical_hit_06.wav",
 }
