dff473976b5ce611c3be4493235750268c0f8e33
OchdasisjaSuppi <08_15Gamer@posteo.at>
2025-04-14 20:37:41 +0200

    Added 6 Soundbites & Adjusted volume level

--- sound.lua
+++ sound.lua
@@ -2,10 +2,21 @@
 
 nova.require "data/lua/jh/sound/common"
 
+register_sound "vo_death"
+{
+	group = "voice",
+	priority = true,
+	volume = 0.9,
+	"data/sound/vo/death/death_01.wav",
+	"data/sound/vo/death/death_02.wav",
+	"data/sound/vo/death/death_03.wav",
+	"data/sound/vo/death/death_05.wav",
+}
+
 register_sound "flesh_bullet_critical_hit"
 {
 	group = "fx",
-	volume = 0.8,
+	volume = 0.9,
 	"data/sound/being/summoner/summoner_melee_01_01.wav",
 	"data/sound/being/summoner/summoner_melee_01_02.wav",
 	"data/sound/being/summoner/summoner_melee_01_03.wav",
@@ -17,7 +28,7 @@ register_sound "flesh_bullet_critical_hit"
 register_sound "flesh_blunt_critical_hit"
 {
 	group = "fx",
-	volume = 0.8,
+	volume = 0.9,
 	"data/sound/event/flesh_blunt_critical_hit/flesh_blunt_critical_hit_01.wav",
 	"data/sound/event/flesh_blunt_critical_hit/flesh_blunt_critical_hit_02.wav",
 	"data/sound/event/flesh_blunt_critical_hit/flesh_blunt_critical_hit_03.wav",
@@ -29,19 +40,20 @@ register_sound "flesh_blunt_critical_hit"
 register_sound "metal_blunt_critical_hit"
 {
 	group = "fx",
-	volume = 0.8,
+	volume = 0.9,
 	"data/sound/event/metal_blunt_critical_hit/metal_blunt_critical_hit_01.wav",
 	"data/sound/event/metal_blunt_critical_hit/metal_blunt_critical_hit_02.wav",
 	"data/sound/event/metal_blunt_critical_hit/metal_blunt_critical_hit_03.wav",
 	"data/sound/event/metal_blunt_critical_hit/metal_blunt_critical_hit_04.wav",
 	"data/sound/event/metal_blunt_critical_hit/metal_blunt_critical_hit_05.wav",
 	"data/sound/event/metal_blunt_critical_hit/metal_blunt_critical_hit_06.wav",
+	"data/sound/event/flesh_blunt_critical_hit/flesh_blunt_critical_hit_07.wav",
 }
 
 register_sound "flesh_blade_critical_hit"
 {
 	group = "fx",
-	volume = 0.8,
+	volume = 0.85,
 	"data/sound/event/flesh_blade_critical_hit/flesh_blade_critical_hit_01.wav",
 	"data/sound/event/flesh_blade_critical_hit/flesh_blade_critical_hit_02.wav",
 	"data/sound/event/flesh_blade_critical_hit/flesh_blade_critical_hit_03.wav",
@@ -53,7 +65,7 @@ register_sound "flesh_blade_critical_hit"
 register_sound "metal_blade_critical_hit"
 {
 	group = "fx",
-	volume = 0.8,
+	volume = 0.9,
 	"data/sound/event/metal_blade_critical_hit/metal_blade_critical_hit_01.wav",
 	"data/sound/event/metal_blade_critical_hit/metal_blade_critical_hit_02.wav",
 	"data/sound/event/metal_blade_critical_hit/metal_blade_critical_hit_03.wav",
